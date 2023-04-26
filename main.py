a348ef9866a9068ddcbab35ebc70c45eedfae361
Kaue-Francisco <kauesantos.francisco@gmail.com>
2023-04-26 09:57:35 -0300

    Adicionando API

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+API_KEY = "845ec3fb6210c0055287e6bdcc07eae4"
+url = ('https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}&lang=pt_br'))
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
